2c5b2e03058cb834f5fc21f6a7071384ff552e10
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-05-03 14:54:17 -0700

    formatted for easier API usage

--- hr_system/lib/hr_system_web/controllers/employee_api_json.ex
+++ hr_system/lib/hr_system_web/controllers/employee_api_json.ex
@@ -2,14 +2,14 @@ defmodule HrSystemWeb.EmployeeApiJSON do
   def index(%{employees: employees}) do
     %{data:
       for(employee <- employees) do
-        [
-          %{id: employee.id},
-          %{last_name: employee.last_name},
-          %{first_name: employee.first_name},
-          %{hire_date: employee.hire_date},
-          %{department: employee.department}
-        ]
-        # %{id: employee.id, last_name: employee.last_name, first_name: employee.first_name, hire_date: employee.hire_date, department: employee.department}
+        # [
+        #   %{id: employee.id},
+        #   %{last_name: employee.last_name},
+        #   %{first_name: employee.first_name},
+        #   %{hire_date: employee.hire_date},
+        #   %{department: employee.department}
+        # ]
+        %{id: employee.id, last_name: employee.last_name, first_name: employee.first_name, hire_date: employee.hire_date, department: employee.department}
       end
     }
   end
